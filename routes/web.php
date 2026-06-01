a9c3127564f6e7e76dcb3ff0edaa80567dc08daf
khawlamt <mettichikhawla@gmail.com>
2026-06-01 23:15:14 +0100

    Correction des tests

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,19 @@ use App\Http\Controllers\RendezVousController;
 use App\Http\Controllers\MedicamentController;
 use App\Http\Controllers\HospitalController;
 use App\Http\Controllers\ChatbotController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', fn() => redirect()->route('dashboard'));
 
 Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])
+            ->name('profile.edit');
 
+        Route::patch('/profile', [ProfileController::class, 'update'])
+            ->name('profile.update');
+
+        Route::delete('/profile', [ProfileController::class, 'destroy'])
+            ->name('profile.destroy');
     // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
